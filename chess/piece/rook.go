d75bb2825ea1fe3d68b7d2c676387e79e6eec295
0xRuFFy <konstantinopora@gmail.com>
2022-09-20 12:50:48 +0200

    added PosMoves for th rook

--- chess/piece/rook.go
+++ chess/piece/rook.go
@@ -6,7 +6,57 @@ type Rook struct {
 }
 
 func (rook Rook) PosMoves(board []Piece) (int, []int) {
-	panic("not implemented")
+	var moves []int
+	var row, col, i int
+
+	for i = rook.position/8 + 1; i < 8; i++ {
+		row = 8 * i
+		col = rook.position % 8
+		if board[row+col] != nil {
+			if board[row+col].GetColor() != rook.color {
+				moves = append(moves, row+col)
+			}
+			break
+		}
+		moves = append(moves, row+col)
+	}
+
+	for i = rook.position/8 - 1; i >= 0; i-- {
+		row = 8 * i
+		col = rook.position % 8
+		if board[row+col] != nil {
+			if board[row+col].GetColor() != rook.color {
+				moves = append(moves, row+col)
+			}
+			break
+		}
+		moves = append(moves, row+col)
+	}
+
+	for i = rook.position%8 + 1; i < 8; i++ {
+		row = rook.position / 8 * 8
+		if board[row+i] != nil {
+			if board[row+i].GetColor() != rook.color {
+				moves = append(moves, row+i)
+			}
+			break
+		}
+		moves = append(moves, row+i)
+	}
+
+	for i = rook.position%8 - 1; i >= 0; i-- {
+		row = rook.position / 8 * 8
+		if board[row+i] != nil {
+			if board[row+i].GetColor() != rook.color {
+				moves = append(moves, row+i)
+			}
+			break
+		}
+		moves = append(moves, row+i)
+	}
+
+	//panic("not implemented")
+	return rook.position, moves
 }
 
 func (rook Rook) ToString() string {
